c93dec4d0aadea29b1a18aaad5738b6563cf514a
Keaton Sentak <k.sentak@outlook.com>
2020-09-28 13:58:19 -0400

    add shuffled array

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
 	const grid = document.querySelector('.grid');
 	let width = 10;
 	let squares = [];
+	let bombCount = 20;
 
-	// Create Board
+	// Create board
 	function createBoard() {
+		// Shuffle game with random bombs
+		const bombsArray = Array(bombCount).fill('bomb');
+		const emptyArray = Array(width * width - bombCount).fill('valid');
+		const gameArray = emptyArray.concat(bombsArray);
+		const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
 		for (let i = 0; i < width * width; i++) {
 			const square = document.createElement('div');
 			square.setAttribute('id', i);
+			square.classList.add(shuffledArray[i]);
 			grid.appendChild(square);
 			squares.push(square);
 		}
